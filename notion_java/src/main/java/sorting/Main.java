9ceba104ce685565eba553f30062c2e7831b8bfb
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-11-23 02:37:54 +0900

    double sorting

--- notion_java/src/main/java/sorting/Main.java
+++ notion_java/src/main/java/sorting/Main.java
@@ -1,2 +1,39 @@
-package sorting;public class Main {
+package sorting;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 오름 차순 Comparator 정렬
+        List<Integer> list = Arrays.asList(3, 10, 5, 7, 4, 2, 6, 9, 1, 7, 8);
+        Collections.sort(list, (a, b) -> {
+            if (a > b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else return -1;
+        });
+        System.out.println(list);
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("김선아", 15, 1));
+        students.add(new Student("나희정", 15, 2));
+        students.add(new Student("김윤종", 15, 3));
+        students.add(new Student("김현철", 15, 1));
+        students.add(new Student("강나영", 15, 2));
+        students.add(new Student("송근석", 16, 3));
+        students.add(new Student("이수한", 16, 1));
+        students.add(new Student("정지환", 16, 2));
+        students.add(new Student("나영", 17, 3));
+        students.add(new Student("신하나", 17, 1));
+
+        students.sort(Comparator.comparing(Student::getClassroom)
+                .thenComparing(Student::getAge, Comparator.reverseOrder())
+                .thenComparing(Student::getName));
+
+        System.out.println(students); // classroom:asc, age: desc, name: asc
+
+        Collections.sort(students, new Student());
+        System.out.println(students); // classroom:desc, age: asc, name: desc
+    }
 }
--- notion_java/src/main/java/sorting/Student.java
+++ notion_java/src/main/java/sorting/Student.java
@@ -1,2 +1,54 @@
-package sorting;public class Student {
+package sorting;
+
+import java.util.Comparator;
+
+class Student implements Comparator<Student> {
+    String name;
+    int age;
+    int classroom;
+
+    public Student(String name, int age, int classroom) {
+        this.name = name;
+        this.age = age;
+        this.classroom = classroom;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getClassroom() {
+        return classroom;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", classroom=" + classroom +
+                '}' + '\n';
+    }
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        if (o1.classroom < o2.classroom)
+            return 1;
+        else if (o1.classroom == o2.classroom) {
+            if (o1.age > o2.age)
+                return 1;
+            else if (o1.age == o2.age) {
+                if (o1.name.compareTo(o2.name) == -1)
+                    return 1;
+                else return -1;
+            } else return -1;
+        } else return -1;
+    }
 }
