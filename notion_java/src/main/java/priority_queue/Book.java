92cff61dd686bd4d93f090161e54d2ad63cc3e02
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-11-23 02:38:21 +0900

    priorityQueue with Comparator

--- notion_java/src/main/java/priority_queue/Book.java
+++ notion_java/src/main/java/priority_queue/Book.java
@@ -1,2 +1,45 @@
-package priority_queue;public class Book {
+package priority_queue;
+
+import java.util.Comparator;
+import java.util.Date;
+
+public class Book implements Comparator<Book> {
+    private long isbn;
+    private String name;
+    private Date updated;
+
+    public Book() {
+    }
+
+    public Book(long isbn, String name, Date updated) {
+        this.isbn = isbn;
+        this.name = name;
+        this.updated = updated;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn=" + isbn +
+                ", name='" + name + '\'' +
+                ", updated=" + updated +
+                '}'+'\n';
+    }
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        if (o1.isbn > o2.isbn)
+            return 1;
+        else if(o1.isbn == o2.isbn){
+            if (o1.updated.before(o2.updated)) // o1가 o2보다 먼저 오면 True - 내림차순 before return 1 - 오름차순 after return 1
+                return 1;
+            else if(o1.updated == o2.updated){
+                if (o1.name.compareTo(o2.name) > 0)
+                    return 1;
+                else return -1;
+            }
+            else return -1;
+        }
+        else return -1;
+    }
 }
--- notion_java/src/main/java/priority_queue/JavaPriorityQueue.java
+++ notion_java/src/main/java/priority_queue/JavaPriorityQueue.java
@@ -1,7 +1,44 @@
 package priority_queue;
 
-public class PriorityQueue {
-    public static void main(String[] args) {
-        
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class JavaPriorityQueue {
+    public static void main(String[] args) throws ParseException {
+        PriorityQueue<Integer> ascendingPriorityQueue = new PriorityQueue<>(); // 오름차순
+        PriorityQueue<Integer> descendingPriorityQueue = new PriorityQueue<>(
+                Collections.reverseOrder()
+        ); // 내림차순
+
+        for (int i = 1; i <= 10; i++) {
+            ascendingPriorityQueue.add(i);
+            descendingPriorityQueue.add(i);
+        }
+        ascendingPriorityQueue.offer(100);
+        descendingPriorityQueue.offer(200);
+
+        for (int i = 1; i <= 11; i++) {
+            System.out.println(ascendingPriorityQueue.poll() + " " + descendingPriorityQueue.poll());
+        }
+
+        ascendingPriorityQueue.add(10);
+        System.out.println("peek: " + ascendingPriorityQueue.peek());
+        System.out.println("element: " + ascendingPriorityQueue.element());
+        try {
+            System.out.println("remove: " + ascendingPriorityQueue.remove());
+            System.out.println("remove: " + ascendingPriorityQueue.remove());
+        } catch (Exception e) {
+            System.out.println("Empty: " + e.getMessage());
+        }
+        ascendingPriorityQueue.clear();
+        System.out.println("size: " + ascendingPriorityQueue.size());
+
+        PriorityQueue<Book> books = new PriorityQueue<>(new Book());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+        books.add(new Book(656454384, "소년만화", dateFormat.parse("2022-01-23")));
+        books.add(new Book(656454383, "오컬트", dateFormat.parse("2022-01-24")));
+
+        System.out.println(books);
     }
 }
