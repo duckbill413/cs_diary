5718cccdb6ff0403d0289f7a7841096a63144c54
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-11-25 16:04:42 +0900

    spring aop

--- spring/spring-aop/src/main/java/com/example/springaop/SpringAopApplication.java
+++ spring/spring-aop/src/main/java/com/example/springaop/SpringAopApplication.java
@@ -3,11 +3,15 @@ package com.example.springaop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 @SpringBootApplication
 public class SpringAopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAopApplication.class, args);
+        System.out.println(Base64.getEncoder().encodeToString("mazingga102".getBytes(StandardCharsets.UTF_8)));
     }
 
 }
--- spring/spring-aop/src/main/java/com/example/springaop/controller/RestApiController.java
+++ spring/spring-aop/src/main/java/com/example/springaop/controller/RestApiController.java
@@ -1,5 +1,7 @@
 package com.example.springaop.controller;
 
+import com.example.springaop.anotation.Decode;
+import com.example.springaop.anotation.Timer;
 import com.example.springaop.dto.User;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,19 @@ public class RestApiController{
     @PostMapping("/post")
     public User post(@RequestBody User user){
         System.out.println("post method" + user);
+        return user;
+    }
+
+    @Timer
+    @DeleteMapping("/delete")
+    public void delete() throws InterruptedException {
+        Thread.sleep(1000 * 2);
+    }
+
+    @Decode
+    @PutMapping("/put")
+    public User put(@RequestBody User user){
+        System.out.println("put method" + user);
         return user;
     }
 }
