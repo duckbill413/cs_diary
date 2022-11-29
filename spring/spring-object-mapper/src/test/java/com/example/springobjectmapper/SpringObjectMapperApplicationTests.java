ed55a08d35aeb5dfe8f4fc893565c136d3bdbdcc
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-11-29 19:34:33 +0900

    object mapper

--- spring/spring-object-mapper/src/test/java/com/example/springobjectmapper/SpringObjectMapperApplicationTests.java
+++ spring/spring-object-mapper/src/test/java/com/example/springobjectmapper/SpringObjectMapperApplicationTests.java
@@ -11,7 +11,6 @@ class SpringObjectMapperApplicationTests {
 
     @Test
     void contextLoads() throws JsonProcessingException {
-        System.out.println("hello-----------");
 
         /* INFO: Json -> Object
         *  Object -> Json
@@ -25,12 +24,13 @@ class SpringObjectMapperApplicationTests {
         // MEMO: object -> text objectMapper가 get method 활용
         // TIP: objectMapper가 get method를 참고하므로 다른 method이름 앞에 get을 붙이면 안됨
         var user = new User("duckbill", 27, "010-1234-1234");
+        System.out.println("System.in: " + user);
+
         var text = objectMapper.writeValueAsString(user);
-        System.out.println(text);
+        System.out.println("Json Text: " + text);
 
         // MEMO: text -> object 기본생성자로 초기화 필요
         var objectUser = objectMapper.readValue(text, User.class);
-        System.out.println(objectUser);
+        System.out.println("System.out: " + objectUser);
     }
-
 }
