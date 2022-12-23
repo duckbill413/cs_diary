0efd28d20f1cf3e28e0586c5155abe85d46e56ac
duckbillLvr <uhyeon7399@daum.net>
2022-12-24 00:21:51 +0900

    java 12 switch lamda

--- notion_java/src/main/java/switch_case/SwitchCase.java
+++ notion_java/src/main/java/switch_case/SwitchCase.java
@@ -1,2 +1,26 @@
-package switch_case;public class SwitchCase {
+package switch_case;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        String sector = "A구역";
+        switch (sector) {
+            case "A구역", "B구역", "C구역" -> System.out.println("지역 1");
+            case "D구역", "E구역", "F구역" -> System.out.println("지역 2");
+            case "G구역", "H구역", "I구역" -> System.out.println("지역 3");
+            default -> System.out.println("지역 설정 오류");
+        }
+
+        String animal = "squirrel";
+        String group = switch (animal){
+            case "Tiger", "Lion" -> "mammal";
+            case "mice", "squirrel" -> "rodents";
+            case "chicken", "duck" -> "birds";
+            case "butterfly", "mantis" -> "insects";
+            default -> {
+                int len = animal.length();
+                yield String.valueOf(len);
+            }
+        };
+        System.out.println(group);
+    }
 }
