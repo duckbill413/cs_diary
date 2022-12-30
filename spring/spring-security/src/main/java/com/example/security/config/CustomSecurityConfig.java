105424e855a8a206ac37e8a41fca24314a70e0ad
duckbillLvr <uhyeon7399@daum.net>
2022-12-31 03:25:25 +0900

    spring security config

--- spring/spring-security/src/main/java/com/example/security/config/CustomSecurityConfig.java
+++ spring/spring-security/src/main/java/com/example/security/config/CustomSecurityConfig.java
@@ -5,8 +5,11 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,31 +22,55 @@ import javax.sql.DataSource;
 
 @Log4j2
 @Configuration
-//@EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
+//@EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CustomSecurityConfig {
     private final DataSource dataSource;
     private final CustomUserDetailService customUserDetailService;
+    private final CustomAuthDetails customAuthDetails;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        log.info("------------configure-------------------");
-
         //커스텀 로그인 페이지
-        http.formLogin().loginPage("/member/login");
-        //CSRF 토큰 비활성화
-        http.csrf().disable();
+        // 헤더, csrf 설정
+        http
+                .headers().disable();
+//                .csrf().disable();
+        // request 권한 설정
+        http.authorizeRequests(request -> request
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+        );
+        // 로그인 설정
+        http.formLogin().loginPage("/login").permitAll()
+                .defaultSuccessUrl("/", false)
+                .failureUrl("/login-error")
+                .authenticationDetailsSource(customAuthDetails);
+        // 로그아웃 설정
+        http.logout().logoutSuccessUrl("/");
+        // Exception 설정
+        http.exceptionHandling().accessDeniedPage("/access-denied");
+
         // remember_me 자동로그인
         http.rememberMe()
                 .key("123456789")
                 .tokenRepository(persistentTokenRepository())
                 .userDetailsService(customUserDetailService)
-                .tokenValiditySeconds(60*60*24*30); // 30일의 유효기간
+                .tokenValiditySeconds(60 * 60 * 24 * 30); // 30일의 유효기간
 
         return http.build();
     }
@@ -51,14 +78,13 @@ public class CustomSecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         // FEAT : Security 작업이 필요없는 css, js 등의 정적 자원들을 필터에서 제외
-        log.info("------------------web configure---------------------");
         return web -> web.ignoring()
-                .antMatchers("/swagger-ui/", "/home/**")
+                .antMatchers("/swagger-ui/")
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Bean
-    public PersistentTokenRepository persistentTokenRepository(){
+    public PersistentTokenRepository persistentTokenRepository() {
         JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
         repo.setDataSource(dataSource);
         return repo;
