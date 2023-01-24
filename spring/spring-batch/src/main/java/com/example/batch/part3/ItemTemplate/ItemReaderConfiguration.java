aaa9022f194a9cb4106929d701c6721f4c3f6a78
duckbillLvr <uhyeon7399@daum.net>
2023-01-24 13:49:29 +0900

    CustomItemReader

---
+++ spring/spring-batch/src/main/java/com/example/batch/part3/ItemTemplate/ItemReaderConfiguration.java
@@ -0,0 +1,59 @@
+package com.example.batch.part3.ItemTemplate;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * author        : duckbill413
+ * date          : 2023-01-24
+ * description   :
+ * ListItemReader 과 같은 기능을 재현
+ **/
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class ItemReaderConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job itemReaderJob() {
+        return this.jobBuilderFactory.get("itemReaderJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.customItemReaderStep())
+                .build();
+    }
+
+    @Bean
+    public Step customItemReaderStep() {
+        return this.stepBuilderFactory.get("customItemReaderStep")
+                .<Person, Person>chunk(10)
+                .reader(new CustomItemReader<>(getItems()))
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemWriter<Person> itemWriter() {
+        return items -> log.info(items.stream().map(Person::getName).collect(Collectors.joining(", ")));
+    }
+
+    private List<Person> getItems() {
+        List<Person> items= new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            items.add(new Person(i+1, "test name" + i, "test age", "test address"));
+        }
+        return items;
+    }
+}
