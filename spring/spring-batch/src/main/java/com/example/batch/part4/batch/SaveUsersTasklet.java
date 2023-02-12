a2a8d9a4a87d074f55fdc2e71210b2fc8e9f7c6a
duckbillLvr <uhyeon7399@daum.net>
2023-02-12 21:06:59 +0900

    fix. User 및 Order 정보 수정

--- spring/spring-batch/src/main/java/com/example/batch/part4/batch/SaveUsersTasklet.java
+++ spring/spring-batch/src/main/java/com/example/batch/part4/batch/SaveUsersTasklet.java
@@ -1,5 +1,6 @@
 package com.example.batch.part4.batch;
 
+import com.example.batch.part5.Orders;
 import com.example.batch.part4.model.Users;
 import com.example.batch.part4.model.UsersRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -34,7 +36,33 @@ public class SaveUsersTasklet implements Tasklet {
     private List<Users> createUsers(){
         List<Users> users = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            Users user = new Users("user"+(i+1), i*6000);
+            Users user = new Users("user"+(i+1),
+                    Collections.singletonList(Orders.builder()
+                            .itemName("item " + i)
+                            .price(i*6000)
+                            .createdDate(LocalDate.now().minusDays(1))
+                            .build()
+                    ));
+            users.add(user);
+        }
+        for (int i = 100; i < 200; i++) {
+            Users user = new Users("user"+(i+1),
+                    Collections.singletonList(Orders.builder()
+                            .itemName("item " + i)
+                            .price(i*2000)
+                            .createdDate(LocalDate.now())
+                            .build()
+                    ));
+            users.add(user);
+        }
+        for (int i = 200; i < 300; i++) {
+            Users user = new Users("user"+(i+1),
+                    Collections.singletonList(Orders.builder()
+                            .itemName("item " + i)
+                            .price(i*800)
+                            .createdDate(LocalDate.now().plusDays(2))
+                            .build()
+                    ));
             users.add(user);
         }
         return users;
