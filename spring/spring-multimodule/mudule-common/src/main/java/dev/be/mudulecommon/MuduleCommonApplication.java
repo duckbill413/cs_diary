1aae03e07daf55f03056212c75cdc7860c56a988
duckbill413 <uhyeon7399@daum.net>
2024-05-25 21:52:34 +0900

    spring module common project init

---
+++ spring/spring-multimodule/mudule-common/src/main/java/dev/be/mudulecommon/MuduleCommonApplication.java
@@ -0,0 +1,13 @@
+package dev.be.mudulecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuduleCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuduleCommonApplication.class, args);
+    }
+
+}
---
+++ spring/spring-multimodule/mudule-common/src/test/java/dev/be/mudulecommon/MuduleCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.mudulecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuduleCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
