6d7b507ce6e32988366019b490675975093df378
duckbill413 <uhyeon7399@daum.net>
2024-05-28 16:43:34 +0900

    multimodule common exception handler

--- spring/spring-multimodule/module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ spring/spring-multimodule/module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,5 +1,7 @@
 package dev.be.moduleapi.service;
 
+import dev.be.moduleapi.exception.CustomException;
+import dev.be.modulecommon.enums.CodeEnum;
 import dev.be.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,5 +17,13 @@ public class DemoService {
 
     public String find() {
         return "find";
+    }
+
+    public String exception() {
+        if (true) {
+            throw new CustomException(CodeEnum.UNKNOWN_ERROR);
+        }
+
+        return "exception";
     }
 }
