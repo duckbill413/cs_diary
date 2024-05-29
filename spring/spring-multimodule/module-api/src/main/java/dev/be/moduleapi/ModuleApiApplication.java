6560b86f43a110e53e6248eda99ffeb5090c60da
duckbill413 <uhyeon7399@daum.net>
2024-05-29 09:19:23 +0900

    module jpa EnableJpaRepository by path

--- spring/spring-multimodule/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ spring/spring-multimodule/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         scanBasePackages = {"dev.be.moduleapi", "dev.be.modulecommon"}
 )
 @EntityScan("dev.be.modulecommon.domain")
-@EnableJpaRepositories()
+@EnableJpaRepositories({"dev.be.modulecommon.repositories"})
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
