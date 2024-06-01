4e4c0d27a45a1f9ecd2a7e43fd5743dee40cd69d
duckbill413 <uhyeon7399@daum.net>
2024-06-02 00:02:13 +0900

    Spring Async

---
+++ spring/spring-async/src/main/java/com/example/springasync/service/AsyncFutureService.java
@@ -0,0 +1,34 @@
+package com.example.springasync.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+@Service
+public class AsyncFutureService {
+
+    public String hello(){
+        for(int i=0; i<5; i++){
+            try {
+                log.info(String.valueOf(i+1));
+                Thread.sleep(2000);
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        return "async hello " + LocalDateTime.now();
+    }
+    @Async("async-thread")
+    public CompletableFuture<String> run() throws ExecutionException, InterruptedException {
+        log.info("now running");
+        return CompletableFuture.completedFuture(hello());
+//        return new AsyncResult(hello()).completable(); // Deprecated
+    }
+}
