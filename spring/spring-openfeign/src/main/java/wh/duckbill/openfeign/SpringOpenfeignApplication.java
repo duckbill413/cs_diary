ca891e68f2d458e601cbefb918770441f57b5dca
duckbill413 <uhyeon7399@daum.net>
2024-06-02 19:36:48 +0900

    spring feign client init

---
+++ spring/spring-openfeign/src/main/java/wh/duckbill/openfeign/SpringOpenfeignApplication.java
@@ -0,0 +1,13 @@
+package wh.duckbill.openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenfeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOpenfeignApplication.class, args);
+    }
+
+}
---
+++ spring/spring-openfeign/src/test/java/wh/duckbill/openfeign/SpringOpenfeignApplicationTests.java
@@ -0,0 +1,13 @@
+package wh.duckbill.openfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenfeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
