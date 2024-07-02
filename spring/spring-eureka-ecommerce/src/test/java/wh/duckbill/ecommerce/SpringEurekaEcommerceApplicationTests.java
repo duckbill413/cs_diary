bcbb3e48a3c0cebd066088ec3bfd0aeb368e167b
duckbill413 <uhyeon7399@daum.net>
2024-07-02 22:18:27 +0900

    spring eureka e-commerce init

---
+++ spring/spring-eureka-ecommerce/src/test/java/wh/duckbill/ecommerce/SpringEurekaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package wh.duckbill.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
