4b30646578f3527b301a31d62fff60338804f8ad
duckbill413 <uhyeon7399@daum.net>
2024-07-16 23:40:37 +0900

    Session 7. Users Microservice 2

--- spring/users-microservice/user-service/src/main/java/wh/duckbill/userservice/jpa/UsersRepository.java
+++ spring/users-microservice/user-service/src/main/java/wh/duckbill/userservice/jpa/UsersRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByUserId(String userId);
+
+    UserEntity findByEmail(String email);
 }
--- spring/users-microservice/user-service/src/main/java/wh/duckbill/userservice/service/UsersServiceImpl.java
+++ spring/users-microservice/user-service/src/main/java/wh/duckbill/userservice/service/UsersServiceImpl.java
@@ -2,6 +2,8 @@ package wh.duckbill.userservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,9 +19,31 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class UsersServiceImpl implements UsersService {
-    private final UsersRepository usersRepository;
+    private final UsersRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
     private final ModelMapper modelMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username + ": not found");
+        }
+        return new User(user.getEmail(), user.getEncryptedPwd(),
+                true, true, true, true,
+                new ArrayList<>());
+    }
+
+    @Override
+    public UserDto getUserDetailsByEmail(String email) {
+        UserEntity userEntity = userRepository.findByEmail(email);
+
+        if (userEntity == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return modelMapper.map(userEntity, UserDto.class);
+    }
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -27,14 +51,14 @@ public class UsersServiceImpl implements UsersService {
 
         UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
         userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
-        usersRepository.save(userEntity);
+        userRepository.save(userEntity);
 
         return modelMapper.map(userEntity, UserDto.class);
     }
 
     @Override
     public UserDto getUserByUserId(String userId) {
-        UserEntity userEntity = usersRepository.findByUserId(userId);
+        UserEntity userEntity = userRepository.findByUserId(userId);
 
         if (userEntity == null) {
             throw new UsernameNotFoundException("User not found!");
@@ -48,6 +72,6 @@ public class UsersServiceImpl implements UsersService {
 
     @Override
     public Iterable<UserEntity> getUserByAll() {
-        return usersRepository.findAll();
+        return userRepository.findAll();
     }
 }
