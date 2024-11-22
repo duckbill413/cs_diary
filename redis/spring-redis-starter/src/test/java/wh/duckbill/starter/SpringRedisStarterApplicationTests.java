50c38c6fb174e8a7b0046818042a5a2155d6e2ff
duckbill413 <uhyeon7399@daum.net>
2024-11-22 23:12:23 +0900

    redis starter initialize

---
+++ redis/spring-redis-starter/src/test/java/wh/duckbill/starter/SpringRedisStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package wh.duckbill.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
