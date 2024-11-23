2c9fb999289d48d09799ab59889c4710b3bf1ad1
duckbill413 <uhyeon7399@daum.net>
2024-11-23 22:43:19 +0900

    Redis 연동 개발

--- redis/spring-redis-starter/src/main/java/wh/duckbill/starter/HelloController.java
+++ redis/spring-redis-starter/src/main/java/wh/duckbill/starter/HelloController.java
@@ -1,7 +1,30 @@
 package wh.duckbill.starter;
 
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public HelloController(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @GetMapping("/setFruit")
+    public String setFruit(@RequestParam String name) {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("fruit", name);
+
+        return "saved.";
+    }
+
+    @GetMapping("/getFruit")
+    public String getFruit() {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        return ops.get("fruit");
+    }
 }
