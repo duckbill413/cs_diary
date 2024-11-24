f8782998d5e05e222e794cd8c52ea0646371b0f9
duckbill413 <uhyeon7399@daum.net>
2024-11-24 22:18:15 +0900

    Reids를 사용한 세션 클러스터링

--- redis/spring-redis-starter/src/main/java/wh/duckbill/starter/LoginController.java
+++ redis/spring-redis-starter/src/main/java/wh/duckbill/starter/LoginController.java
@@ -5,20 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-
 @RestController
 public class LoginController {
-    private final HashMap<String, String> sessionMap = new HashMap<>();
 
     @GetMapping("/login")
     public String login(@RequestParam String name, HttpSession session) {
-        sessionMap.put(session.getId(), name);
+        session.setAttribute("name", name);
         return "saved.";
     }
 
     @GetMapping("/myName")
     public String myName(HttpSession session) {
-        return sessionMap.get(session.getId());
+        return (String) session.getAttribute("name");
     }
 }
