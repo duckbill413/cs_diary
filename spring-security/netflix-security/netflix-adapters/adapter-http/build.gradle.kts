59d0d6d489bfa1da27edefdfa56d7dfaa557e452
duckbill413 <uhyeon7399@daum.net>
2024-12-08 01:51:12 +0900

    netflix security project initialize

--- spring-security/netflix-security/netflix-adapters/adapter-http/build.gradle.kts
+++ spring-security/netflix-security/netflix-adapters/adapter-http/build.gradle.kts
@@ -1,2 +1,5 @@
 dependencies {
+    implementation(project(":netflix-core:core-port"))
+
+    implementation("org.springframework:spring-context")
 }
--- spring-security/netflix-security/netflix-core/core-service/build.gradle.kts
+++ spring-security/netflix-security/netflix-core/core-service/build.gradle.kts
@@ -1,2 +1,8 @@
 dependencies {
+    implementation(project(":netflix-core:core-usecase"))
+    implementation(project(":netflix-core:core-port"))
+
+    runtimeOnly(project(":netflix-adapters:adapter-http"))
+
+    implementation("org.springframework:spring-context")
 }
