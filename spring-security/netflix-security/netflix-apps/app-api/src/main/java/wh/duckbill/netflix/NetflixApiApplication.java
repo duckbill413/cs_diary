81044945d661f48b666b66fdc40763dd663e408d
duckbill413 <uhyeon7399@daum.net>
2024-12-07 23:26:14 +0900

    netflix security project initialize

--- spring-security/netflix-security/netflix-apps/app-api/src/main/java/wh/duckbill/netflix/NetflixApiApplication.java
+++ spring-security/netflix-security/netflix-apps/app-api/src/main/java/wh/duckbill/netflix/NetflixApiApplication.java
@@ -1,4 +1,11 @@
 package wh.duckbill.netflix;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class NetflixApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixApiApplication.class, args);
+    }
 }
