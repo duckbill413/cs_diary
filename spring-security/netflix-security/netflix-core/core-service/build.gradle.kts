99e4dbeff97dc9c0870d527cfa87ae58779cb8f3
duckbill413 <uhyeon7399@daum.net>
2024-12-09 01:36:05 +0900

    MySQL 연동하기

--- spring-security/netflix-security/netflix-core/core-service/build.gradle.kts
+++ spring-security/netflix-security/netflix-core/core-service/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
     implementation(project(":netflix-core:core-port"))
 
     runtimeOnly(project(":netflix-adapters:adapter-http"))
+    runtimeOnly(project(":netflix-adapters:adapter-persistence"))
 
     implementation("org.springframework:spring-context")
 }
