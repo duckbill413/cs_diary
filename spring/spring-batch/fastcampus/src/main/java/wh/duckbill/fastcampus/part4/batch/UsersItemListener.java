4e5101648f0e49257996fb74e31d7d3fb5ce2db4
duckbill <uhyeon7399@daum.net>
2025-03-31 02:27:47 +0900

    Spring batch Fastcampus Refactoring Start

---
+++ spring/spring-batch/fastcampus/src/main/java/wh/duckbill/fastcampus/part4/batch/UsersItemListener.java
@@ -0,0 +1,32 @@
+package wh.duckbill.fastcampus.part4.batch;
+
+import com.example.batch.part4.model.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.annotation.AfterJob;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Date;
+
+/**
+ * author        : duckbill413
+ * date          : 2023-02-07
+ * description   :
+ **/
+@Slf4j
+@RequiredArgsConstructor
+public class UsersItemListener {
+    private final UsersRepository usersRepository;
+    @AfterJob
+    public void showJobResult(JobExecution jobExecution){
+        Collection<Object> users = usersRepository.findAllByUpdatedDate(LocalDate.now());
+
+        Date startTime = jobExecution.getStartTime();
+        Date endTime = jobExecution.getEndTime();
+        long totalTime = endTime.getTime() - startTime.getTime();
+
+        log.info("총 데이터 처리 {}건, 처리 시간 : {}millis", users.size(), totalTime);
+    }
+}
